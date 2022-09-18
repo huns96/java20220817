66068000143f513a7d7f1358a2fb32793c2eb2d1
huns96 <huns7610@naver.com>
2022-09-18 10:39:54 +0900

    0918-1

--- src/ch11/book/P519/PatternExample.java
+++ src/ch11/book/P519/PatternExample.java
@@ -9,13 +9,18 @@ public class PatternExample {
 		
 		boolean result = Pattern.matches(regExp, data);
 		if(result) {
-			System.out.println("정규식과 일치합니다");
+			System.out.println("정규식과 일치합니다.");
 		} else {
-			System.out.println("정규식과 일치하지 않습니다");
+			System.out.println("정규식과 일치하지 않습니다.");
 		}
 		
 		regExp = "\\w+@\\w+\\.\\w(\\.\\w+)?";
 		data = "angel@navercom";
-		result
+		result = Pattern.matches(regExp, data);
+		if (result) {
+			System.out.println("정규식과 일치합니다.");
+		} else {
+			System.out.println("정규식과 일치하지 않습니다.");
+		}
 	}
 }
--- src/ch11/lecture/p05class/C02Reflection.java
+++ src/ch11/lecture/p05class/C02Reflection.java
@@ -1,11 +1,16 @@
 package ch11.lecture.p05class;
 
+import java.lang.reflect.Field;
+
 public class C02Reflection {
 	public static void main(String[] args) {
 		Class c = String.class;
 		
+		System.out.println("필드 목록");
 		Field[] fields = c.getFields();
 		
-		for (Field field : fields)
+		for (Field field : fields) {
+			System.out.println(field.get());
+		}
 	}
 }
